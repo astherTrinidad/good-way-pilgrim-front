155b3f48ab0be26740eb57d14058b91f13e52419
astherTrinidad <asther.tmora@gmail.com>
2021-05-11 15:16:31 +0200

    cambio de páginas rectificada

--- src/components/pages/login/view.js
+++ src/components/pages/login/view.js
@@ -85,8 +85,8 @@ export default function Login() {
 
           const achievementUserLength =
             datos.achievements.length !== 0 || datos.paths.length !== 0
-              ? history.replace(appRoutes.meProfile)
-              : history.replace(appRoutes.meProfileData);
+              ? history.replace(appRoutes.meProfileData)
+              : history.replace(appRoutes.meProfile);
         }
 
         setIsfetching(false);
