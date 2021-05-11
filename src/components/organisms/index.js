8fe4539819032c24507f725a802b65540aaa2f69
Irene_Cloud <irene.sanchez@clouddistrict.com>
2021-05-11 15:11:38 +0200

    diferenciacion perfil con datos y sin datos

--- src/components/organisms/index.js
+++ src/components/organisms/index.js
@@ -1,3 +1,3 @@
-export { default as Navbar } from './navbar/view';
+export { default as Navbar } from './navBar/view';
 export { default as Footer } from './Footer/view';
 export { default as InfoSectionOneColumn } from './InfoSectionOneColumn/view';
